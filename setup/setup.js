3aabd65a0059e93dcb2ad43d2b03d3c14cb89b52
Firose Hussain <github@firumon.com>
2019-04-16 02:09:55 +0530

    For electron-windows-installer

---
+++ setup/setup.js
@@ -0,0 +1,24 @@
+const installer = require('electron-installer-windows');
+
+const options = {
+    "src": 'builds/sssync-win32-x64/',
+    "dest": 'setup/installers/',
+    "icon": "assets/icons/win/icon.ico",
+    "name": "sssync",
+    "productName": "ePlus Smart Sale Synchronizer",
+    "description": "Database synchronizer to make local and remote database synchronized",
+    "productDescription": "Database synchronizer to make local and remote database synchronized",
+    "version": "0.1.1",
+    "authors": ["Milestone Innovative Technologies"],
+    "owners": ["Milestone Innovative Technologies"],
+    "noMsi": true
+};
+
+console.log('Creating package (this may take a while)');
+
+installer(options)
+    .then(() => console.log(`Successfully created package at ${options.dest}`))
+    .catch(err => {
+        console.error(err, err.stack);
+        process.exit(1);
+    });
